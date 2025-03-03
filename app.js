3941765e797f3e621dff1f586caaaea4c4e80272
Manikandan <manimk1704@gmail.com>
2025-03-03 14:51:57 +0530

    Added websocket connection

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, ()=> {
+  console.log(`Server is running on port ${PORT}`);
+});
+const io = require('socket.io')(server);
+app.use(express.static(path.join(__dirname, 'public')));
+
+let socketsConnected = new Set();
+
+io.on('connection', onConnected);
+
+function onConnected(socket){
+    console.log(socket.id);
+    socketsConnected.add(socket.id);
+    io.emit('clients-total', socketsConnected.size); 
+    socket.on('disconnect', () =>{
+        console.log('Socket disconnected', socket.id)
+        socketsConnected.delete(socket.id);
+        io.emit('clients-total', socketsConnected.size); 
+    })
+}
+
